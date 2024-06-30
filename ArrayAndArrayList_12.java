8ed184977d1f3907b85ed2016b6f950b34e2aae0
Ankit Chowdhury <ankitchowdhury550@gmail.com>
2024-06-30 12:59:51 +0530

    3rd

--- ArrayAndArrayList_12.java
+++ ArrayAndArrayList_12.java
@@ -1,8 +1,12 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ArrayAndArrayList_12 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+
+        //Array of primitves
+
         int[] arr= new int[5];
         arr[0]=13;
         arr[1]=45;
@@ -16,10 +20,41 @@ public class ArrayAndArrayList_12 {
             System.out.println("Enter "+i+" element");
             arr[i]=in.nextInt();
         }
+
         System.out.println("Array elements are coming: ");
         for(int i=0;i<arr.length;i++){
             System.out.print(arr[i]+" ");
             //hello
         }
+
+        System.out.println();
+
+        //simple method of print array elements
+        System.out.println(Arrays.toString(arr));
+
+        System.out.println();
+
+        // enhanced for loop
+        System.out.println("Array elements are coming: by for each loop");
+        for(int num : arr){
+            System.out.print(num +" "); // num represents each element in array
+        }
+
+        //System.out.println(arr[5]); // index out of bound error
+        
+
+        // Array of non primitve type
+        
+        String[] str = new String[4];
+        System.out.println("give array elements");
+        for(int i=0;i<str.length;i++){
+            System.out.print("give "+i+" elemnte");
+            str[i]=in.next();
+        }
+        for (String string : str) {
+            System.out.println(string);
+        }
+        in.close();
     }    
+    
 }
